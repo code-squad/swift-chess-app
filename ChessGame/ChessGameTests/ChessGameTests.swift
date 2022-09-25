d513d377af8ad20e7b93f3e22cf5ae0e4e6886bf
Sunghyun Kim <sunghyun_k@icloud.com>
2022-09-25 21:09:47 +0900

    추가: 실패가능한 테스트케이스 추가

--- ChessGame/ChessGameTests/ChessGameTests.swift
+++ ChessGame/ChessGameTests/ChessGameTests.swift
@@ -16,7 +16,7 @@ final class ChessGameTests: XCTestCase {
         game = ChessGame()
     }
     
-    func testChessBoard() {
+    func testPawnPosition() {
         let board = ChessBoard.standardChessBoard()
         
         for file in 0..<board.filesCount {
@@ -52,6 +52,25 @@ final class ChessGameTests: XCTestCase {
             "A4A5"
         ]
             .forEach { XCTAssert(move($0)) }
+    }
+    
+    func testMovementFails() {
+        // 흰색 말이 두 턴 연속으로 이동
+        XCTAssert(move("A2A3"))
+        XCTAssertFalse(move("A3A4"))
+        // 시작점에 이동시킬 기물이 없는 경우
+        XCTAssertFalse(move("H5H4"))
+    }
+    
+    func testBoardMovementFails() {
+        var board = ChessBoard(files: 8, ranks: 8)
+        board[Position("A2")!] = ChessPiece(type: .pawn, teamColor: .white)
+        board[Position("A3")!] = ChessPiece(type: .pawn, teamColor: .white)
+        
+        // 흰색 폰이 뒤로 이동
+        XCTAssertFalse(board.movePiece(from: .init("A2")!, to: .init("A1")!))
+        // 흰색 폰이 다른 흰색 폰 위치로 이동
+        XCTAssertFalse(board.movePiece(from: .init("A2")!, to: .init("A3")!))
     }
     
     private func move(_ command: String) -> Bool {
