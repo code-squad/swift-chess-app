c88abc53ef31e4f5d028ed1e9bf08e9a798da830
JAEYOUNGYUN <lastmore9@gmail.com>
2022-09-26 21:43:15 +0900

    Add struct Position

---
+++ Chess/Chess/Position.swift
@@ -0,0 +1,13 @@
+//
+//  Position.swift
+//  Chess
+//
+//  Created by jaeyoung Yun on 2022/09/26.
+//
+
+import Foundation
+
+struct Position: Equatable {
+    var fileIndex: Int
+    var rankIndex: Int
+}
