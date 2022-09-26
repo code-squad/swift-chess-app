4a11e319faeadbc21950da836a62ef721bee063e
Beepeach <eh7457303j@gmail.com>
2022-09-27 08:50:14 +0900

    Feat: Chess에 Status와 start 메서드 추가

---
+++ ChessGame/ChessGame/Models/Chess+Status.swift
@@ -0,0 +1,16 @@
+//
+//  Chess+Status.swift
+//  ChessGame
+//
+//  Created by JunHeeJo on 9/27/22.
+//
+
+import Foundation
+
+extension Chess {
+    enum Status {
+        case ready
+        case inProgress
+        case end
+    }
+}
