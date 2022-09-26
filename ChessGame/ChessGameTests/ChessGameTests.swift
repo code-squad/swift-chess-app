c0b56e7fb9ed27c738dfdc65b82ee2f31a0451cb
Sunghyun Kim <sunghyun_k@icloud.com>
2022-09-27 01:40:29 +0900

    테스트 추가: 폰의 이동가능 위치 테스트 추가

--- ChessGame/ChessGameTests/ChessGameTests.swift
+++ ChessGame/ChessGameTests/ChessGameTests.swift
@@ -16,6 +16,17 @@ final class ChessGameTests: XCTestCase {
         game = ChessGame()
     }
     
+    // MARK: - ChessPiece
+    
+    func testAllMovementsOfPawn() {
+        let whitePawn = ChessPiece(type: .pawn, teamColor: .white)
+        XCTAssertEqual(whitePawn.availableMovements(at: .init("A2")!), [.init("A3")!])
+        let blackPawn = ChessPiece(type: .pawn, teamColor: .black)
+        XCTAssertEqual(blackPawn.availableMovements(at: .init("A7")!), [.init("A6")!])
+    }
+    
+    // MARK: - ChessBoard
+    
     func testPawnPosition() {
         let board = ChessBoard.standardChessBoard()
         
@@ -43,6 +54,19 @@ final class ChessGameTests: XCTestCase {
         XCTAssertEqual(blackPawnCount, 8)
     }
     
+    func testBoardMovementFails() {
+        var board = ChessBoard(files: 8, ranks: 8)
+        board[Position("A2")!] = ChessPiece(type: .pawn, teamColor: .white)
+        board[Position("A3")!] = ChessPiece(type: .pawn, teamColor: .white)
+        
+        // 흰색 폰이 뒤로 이동
+        XCTAssertFalse(board.movePiece(from: .init("A2")!, to: .init("A1")!))
+        // 흰색 폰이 다른 흰색 폰 위치로 이동
+        XCTAssertFalse(board.movePiece(from: .init("A2")!, to: .init("A3")!))
+    }
+    
+    // MARK: - ChessGame
+    
     func testMovement() {
         func moveAndAssert(_ command: String) {
             let args = Array(command)
@@ -67,17 +91,6 @@ final class ChessGameTests: XCTestCase {
         XCTAssertFalse(move("A3A4"))
         // 시작점에 이동시킬 기물이 없는 경우
         XCTAssertFalse(move("H5H4"))
-    }
-    
-    func testBoardMovementFails() {
-        var board = ChessBoard(files: 8, ranks: 8)
-        board[Position("A2")!] = ChessPiece(type: .pawn, teamColor: .white)
-        board[Position("A3")!] = ChessPiece(type: .pawn, teamColor: .white)
-        
-        // 흰색 폰이 뒤로 이동
-        XCTAssertFalse(board.movePiece(from: .init("A2")!, to: .init("A1")!))
-        // 흰색 폰이 다른 흰색 폰 위치로 이동
-        XCTAssertFalse(board.movePiece(from: .init("A2")!, to: .init("A3")!))
     }
     
     private func move(_ command: String) -> Bool {
