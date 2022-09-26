a3e2896bd3b5f9591c5ab31f3d4bc99481494cc9
Beepeach <eh7457303j@gmail.com>
2022-09-27 08:33:51 +0900

    Add: Board 클래스 및 테스트 추가

---
+++ ChessGame/ChessGameTests/Cases/Models/BoardTests.swift
@@ -0,0 +1,23 @@
+//
+//  BoardTests.swift
+//  ChessGameTests
+//
+//  Created by JunHeeJo on 9/27/22.
+//
+
+import XCTest
+@testable import ChessGame
+
+final class BoardTests: XCTestCase {
+    var sut: Board!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = Board()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+}
