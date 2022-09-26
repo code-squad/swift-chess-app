f4d020bda3e48f3d67ec3069f34b167e5289944e
inddoni <lychee@daangn.com>
2022-09-27 01:56:13 +0900

    🎉 init Project

---
+++ Chess/Chess/Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Lychee Choi on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    
+  }
+
+
+}
+
