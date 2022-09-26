91983e224aa92cc6265f6ec14344e29ff61c626c
dochoi <dochoi@student.42seoul.kr>
2022-09-26 22:37:38 +0900

    프로젝트 초기화

---
+++ ChessApp/ChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by 최동규 on 2022/09/26.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
