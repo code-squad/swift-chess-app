9d0501206555be2c48966a9cab14e6ac0088e83a
RoadtoS7 <kshvv975@gmail.com>
2022-09-28 00:12:40 +0900

    유저의 명령을 입력받는 InputView 구현

--- swift-chess-app/swift-chess-app/InputView.swift
+++ swift-chess-app/swift-chess-app/InputView.swift
@@ -5,4 +5,13 @@
 //  Created by nylah.j on 2022/09/28.
 //
 
-import Foundation
+enum InputView {
+    func readCommand() -> String {
+        print("명령을 입력하세요> ", terminator: "")
+        guard let input = readLine(),
+              input.isEmpty == false else {
+                  return readCommand()
+              }
+        return input
+    }
+}
