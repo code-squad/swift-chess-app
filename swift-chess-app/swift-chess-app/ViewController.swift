4bea796c51e4704db5b876647c54fe2cc667bc09
jaeeunlee322 <jaeeun.lee@wesang.com>
2022-09-28 00:44:06 +0900

    프로젝트 생성

---
+++ swift-chess-app/swift-chess-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-chess-app
+//
+//  Created by 이재은(Lee, Jaeeun) on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
