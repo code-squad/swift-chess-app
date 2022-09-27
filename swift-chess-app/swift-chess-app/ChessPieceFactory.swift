54fcee7ef94f9f896d1ff98778d3dacccb42e5b1
RoadtoS7 <kshvv975@gmail.com>
2022-09-28 00:34:30 +0900

    체스 위치를 가지고 Chess를 생성하는 ChessPieceFactory 구현

--- swift-chess-app/swift-chess-app/ChessPieceFactory.swift
+++ swift-chess-app/swift-chess-app/ChessPieceFactory.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class ChessPieceFactory {
+    static func create(chessPieceName: String, locations: [Location]) -> [ChessPiece]? {
+        switch chessPieceName {
+        case "WhitePawn":
+            return locations.map { location in
+                WhitePawn(location: location)
+            }
+            
+        case "BlackPawn":
+            return locations.map { location in
+                BlackPawn(location: location)
+            }
+        default:
+            return nil
+        }
+    }
+}
