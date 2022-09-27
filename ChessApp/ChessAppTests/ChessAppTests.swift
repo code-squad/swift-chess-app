acc6ab0c51e66481a025704df1e8d6f7594450de
임승혁 <seunghyeok.lim@kurlycorp.com>
2022-09-28 04:24:25 +0900

    feature: 체스보드 display() 함수 기능 구현 및 테스트 코드 작성

--- ChessApp/ChessAppTests/ChessAppTests.swift
+++ ChessApp/ChessAppTests/ChessAppTests.swift
@@ -9,5 +9,30 @@ import XCTest
 @testable import ChessApp
 
 final class ChessAppTests: XCTestCase {
-
+  var board: Board!
+  
+  override func setUp() {
+    super.setUp()
+    
+    self.board = Board()
+  }
+  
+  func testBoardDisplay() {
+    let expectBoard = """
+                       ABCDEFGH
+                      1........
+                      2♟♟♟♟♟♟♟♟
+                      3........
+                      4........
+                      5........
+                      6........
+                      7♙♙♙♙♙♙♙♙
+                      8........
+                       ABCDEFGH
+                      """
+    
+    let displayBoard = self.board.display()
+    
+    XCTAssertEqual(expectBoard, displayBoard)
+  }
 }
