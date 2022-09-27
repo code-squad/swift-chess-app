f33e342d4850c6f1f6d12d39fcbf5fdd113c142a
RoadtoS7 <kshvv975@gmail.com>
2022-09-27 23:26:01 +0900

    initial commit

---
+++ swift-chess-app/swift-chess-app/ChessBoard.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  swift-chess-app
+//
+//  Created by nylah.j on 2022/09/27.
+//
+
+import Foundation
+
+enum ChessConstant {
+    static let rankCount = 8
+    static let fileCount = 8
+    static let maxRank = 8
+    static let minRank = 1
+    static let minFile = "A"
+    static let maxFile = "H"
+}
