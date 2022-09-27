8f1d1ff468636ed1b84b8920dfe2ddf5d19357da
RoadtoS7 <kshvv975@gmail.com>
2022-09-27 23:45:49 +0900

    ChessBoard 구현

--- swift-chess-app/swift-chess-app/ChessBoard.swift
+++ swift-chess-app/swift-chess-app/ChessBoard.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+struct Score {
+    let whitePawnScore: Int
+    let blackPawnScore: Int
+}
+
 struct ChessBoard {
     enum Constant {
         static let rankCount = 8
@@ -16,8 +21,62 @@ struct ChessBoard {
         static let minFile = "A"
         static let maxFile = "H"
         static let rankRange = minRank...maxRank
+        static let pawnsCount = 8
+        static let initalWhiteRank = 7
+        static let initalBlackRank = 2
     }
     
+    enum Error: LocalizedError {
+        case pawnCountWrong
+        case wrongWhitePawnLocation
+        case wrongBlackPawnLocation
+        
+        var errorDescription: String? {
+            switch self {
+            case .pawnCountWrong:
+                return "Pawn는 색상별로 8개만 가능합니다."
+            case .wrongWhitePawnLocation:
+                return "초기 생성 위치는 흑색은 2-rank만 가능합니다."
+            case .wrongBlackPawnLocation:
+                return "초기 생성 위치는 백색은 7-rank만 가능합니다."
+            }
+        }
+    }
+    
+    
+    let whitePawns: [WhitePawn]
+    let blackPawns: [BlackPawn]
+    let value: [[Pawn?]] = .init(repeating: .init(repeating: nil, count: 8), count: 8)
+    
+    
+    init(whitePawns: [WhitePawn], blackPawns: [BlackPawn]) throws {
+        if whitePawns.count > Constant.pawnsCount || blackPawns.count > Constant.pawnsCount {
+            throw Error.pawnCountWrong
+        }
+        
+        let rightWhitePawnCount = whitePawns.filter { $0.location?.rank == Constant.initalWhiteRank }
+            .count
+        if rightWhitePawnCount != Constant.pawnsCount {
+            throw Error.wrongWhitePawnLocation
+        }
+        
+        let rightBlackPawnCount = blackPawns.filter { $0.location?.rank == Constant.initalBlackRank }
+            .count
+        if rightBlackPawnCount != Constant.pawnsCount {
+            throw Error.wrongBlackPawnLocation
+        }
+        
+        self.whitePawns = whitePawns
+        self.blackPawns = blackPawns
+    }
+    
+    func score() -> Score {
+        let whitePawnScore = blackPawns.compactMap { $0.location }
+            .count
+        let blackPawnScore = whitePawns.compactMap { $0.location }
+            .count
+        return .init(whitePawnScore: whitePawnScore, blackPawnScore: blackPawnScore)
+    }
 }
 
 
