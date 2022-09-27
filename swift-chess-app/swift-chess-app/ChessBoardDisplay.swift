2e94f2cb6d9f48bc4b5dd1327d7d8bccb7431799
RoadtoS7 <kshvv975@gmail.com>
2022-09-28 00:06:01 +0900

    ChessBoardFormatter -> ChessBoardDisplay로 이름 수정, format() -> display() 이름 수정

--- swift-chess-app/swift-chess-app/ChessBoardDisplay.swift
+++ swift-chess-app/swift-chess-app/ChessBoardDisplay.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-enum ChessBoardFormatter {
+enum ChessBoardDisplay {
     enum Constant {
         static let blackPawn = "\(String(describing: UnicodeScalar(0x265F)))"
         static let whitePawn = "\(String(describing: UnicodeScalar(0x2659)))"
         static let empty = "."
     }
-    static func format(board: ChessBoard) -> String {
+    static func display(board: ChessBoard) -> String {
         let fileIndex = "ABCDEFG"
         let formattedBoard = board.value.enumerated()
             .map { (index, rank) in
