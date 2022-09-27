a1b8346ab6424e61b5d786470176183ae556a930
inddoni <lychee@daangn.com>
2022-09-27 23:08:17 +0900

    ✨feat: Pawn 이동가능한 위치 반환 메서드 추가

--- Chess/ChessTests/PawnTests.swift
+++ Chess/ChessTests/PawnTests.swift
@@ -33,4 +33,70 @@ final class PawnTests: XCTestCase {
     // then
     XCTAssertEqual("D3", result)
   }
+  
+  
+  // MARK: - moveablePositions
+  
+  func test_moveablePositions_이동할수있는_모든위치를_반환해요() throws {
+    // given
+    let sut = Pawn(
+      color: .black,
+      position: PawnPosition(rank: 1, file: .b)
+    )
+    
+    // when
+    let result = sut.moveablePositions()
+    
+    // then
+    XCTAssertEqual([
+      PawnPosition(rank: 1, file: .a),
+      PawnPosition(rank: 1, file: .c),
+      PawnPosition(rank: 2, file: .b)
+    ], result)
+  }
+  
+  func test_moveablePositions_모서리일_경우_이동할수있는_모든위치를_반환해요() throws {
+    // given
+    let sut = Pawn(
+      color: .black,
+      position: PawnPosition(rank: 1, file: .a)
+    )
+    
+    // when
+    let result = sut.moveablePositions()
+    
+    // then
+    XCTAssertEqual([
+      PawnPosition(rank: 1, file: .b),
+      PawnPosition(rank: 2, file: .a)
+    ], result)
+  }
+  
+  func test_moveablePositions_흑색은_같거나_더_큰_rank로만_이동할_수_있어요() throws {
+    // given
+    let sut = Pawn(
+      color: .black,
+      position: PawnPosition(rank: 1, file: .a)
+    )
+    
+    // when
+    let result = sut.moveablePositions().contains(where: { $0.rank < 1 })
+    
+    // then
+    XCTAssertFalse(result)
+  }
+  
+  func test_moveablePositions_흰색은_같거나_더_작은_rank로만_이동할_수_있어요() throws {
+    // given
+    let sut = Pawn(
+      color: .white,
+      position: PawnPosition(rank: 7, file: .a)
+    )
+    
+    // when
+    let result = sut.moveablePositions().contains(where: { $0.rank > 7 })
+    
+    // then
+    XCTAssertFalse(result)
+  }
 }
