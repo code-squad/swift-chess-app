b7cbfa97f439c43f3bbd0d294e85ba7f406f4299
inddoni <lychee@daangn.com>
2022-09-28 00:08:05 +0900

    ✨feat: Board 기본 속성 추가

---
+++ Chess/ChessTests/BoardTests.swift
@@ -0,0 +1,19 @@
+//
+//  BoardTests.swift
+//  ChessTests
+//
+//  Created by Lychee Choi on 2022/09/27.
+//
+
+import XCTest
+
+class BoardTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+}
