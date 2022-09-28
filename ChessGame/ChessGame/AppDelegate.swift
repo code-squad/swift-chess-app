9b65977c621eae1082ebff46b5b34462c4b08963
lenaios <sue822330@gmail.com>
2022-09-28 18:47:30 +0900

    Board와 테스트 코드 추가

---
+++ ChessGame/ChessGame/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  ChessGame
+//
+//  Created by Ador on 2022/09/27.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
---
+++ ChessGame/ChessGame/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  ChessGame
+//
+//  Created by Ador on 2022/09/27.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
