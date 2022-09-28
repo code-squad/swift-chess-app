e9ae66e9a2647e87f45f4192d95f7e8012c324d0
Geonhee Son <ryan.son1002@gmail.com>
2022-09-29 01:03:59 +0900

    test: `CommandParser` 기능 검증 테스트 메서드 추가

--- SwiftChess/SwiftChessTests/Board/BoardTesting.swift
+++ SwiftChess/SwiftChessTests/Board/BoardTesting.swift
@@ -68,7 +68,15 @@ extension Board.Location {
     static let A0 = Self(rank: 0, file: 1)
     static let A1 = Self(rank: 1, file: 1)
     static let A2 = Self(rank: 2, file: 1)
+    static let A7 = Self(rank: 7, file: 1)
+    static let A8 = Self(rank: 8, file: 1)
+    static let B1 = Self(rank: 1, file: 2)
     static let D4 = Self(rank: 4, file: 4)
     static let D5 = Self(rank: 5, file: 4)
+    static let G1 = Self(rank: 1, file: 7)
+    static let G8 = Self(rank: 8, file: 7)
+    static let H1 = Self(rank: 1, file: 8)
+    static let H7 = Self(rank: 7, file: 8)
+    static let H8 = Self(rank: 8, file: 8)
 }
 // swiftlint:enable comma identifier_name
---
+++ SwiftChess/SwiftChessTests/Board/Component/CommandParserTests.swift
@@ -0,0 +1,122 @@
+//
+//  CommandParserTests.swift
+//  SwiftChessTests
+//
+//  Created by Geonhee on 2022/09/29.
+//
+
+import XCTest
+@testable import SwiftChess
+
+final class CommandParserTests: XCTestCase {
+
+    func test_A1에서A2로이동하는유효한입력을하면_A1시작점과A2목적지로파싱된다() throws {
+        let command = "A1->A2"
+
+        let parsed = try CommandParser.parse(command)
+
+        XCTAssertEqual(parsed.startPoint, .A1)
+        XCTAssertEqual(parsed.endPoint, .A2)
+    }
+
+    func test_A7에서A8로이동하는유효한입력을하면_A7시작점과A8목적지로파싱된다() throws {
+        let command = "A7->A8"
+
+        let parsed = try CommandParser.parse(command)
+
+        XCTAssertEqual(parsed.startPoint, .A7)
+        XCTAssertEqual(parsed.endPoint, .A8)
+    }
+
+    func test_B1에서A1으로이동하는유효한입력을하면_B1시작점과A1목적지로파싱된다() throws {
+        let command = "A1->B1"
+
+        let parsed = try CommandParser.parse(command)
+
+        XCTAssertEqual(parsed.startPoint, .A1)
+        XCTAssertEqual(parsed.endPoint, .B1)
+    }
+
+    func test_G1에서H1으로이동하는유효한입력을하면_G1시작점과H1목적지로파싱된다() throws {
+        let command = "G1->H1"
+
+        let parsed = try CommandParser.parse(command)
+
+        XCTAssertEqual(parsed.startPoint, .G1)
+        XCTAssertEqual(parsed.endPoint, .H1)
+    }
+
+    func test_H7에서H8로이동하는유효한입력을하면_H7시작점과H8목적지로파싱된다() throws {
+        let command = "H7->H8"
+
+        let parsed = try CommandParser.parse(command)
+
+        XCTAssertEqual(parsed.startPoint, .H7)
+        XCTAssertEqual(parsed.endPoint, .H8)
+    }
+
+    func test_G8에서H8로이동하는유효한입력을하면_H7시작점과H8목적지로파싱된다() throws {
+        let command = "G8->H8"
+
+        let parsed = try CommandParser.parse(command)
+
+        XCTAssertEqual(parsed.startPoint, .G8)
+        XCTAssertEqual(parsed.endPoint, .H8)
+    }
+
+    func test_지정되지구분형식으로명령어를입력하면_파싱에실패하여검증실패에러를던진다() throws {
+        let command = "A1>A2"
+
+        XCTAssertThrowsError(try CommandParser.parse(command)) { error in
+            XCTAssertEqual(error as? CommandParserError, .validationFailed)
+        }
+    }
+
+    func test_A1에서A0로이동하는유효하지않은입력을하면_파싱에실패하여검증실패에러를던진다() throws {
+        let command = "A1->A0"
+
+        XCTAssertThrowsError(try CommandParser.parse(command)) { error in
+            XCTAssertEqual(error as? CommandParserError, .validationFailed)
+        }
+    }
+
+    func test_H1에서H0로이동하는유효하지않은입력을하면_파싱에실패하여검증실패에러를던진다() throws {
+        let command = "H1->H0"
+
+        XCTAssertThrowsError(try CommandParser.parse(command)) { error in
+            XCTAssertEqual(error as? CommandParserError, .validationFailed)
+        }
+    }
+
+    func test_H1에서I1으로이동하는유효하지않은입력을하면_파싱에실패하여검증실패에러를던진다() throws {
+        let command = "H1->I1"
+
+        XCTAssertThrowsError(try CommandParser.parse(command)) { error in
+            XCTAssertEqual(error as? CommandParserError, .validationFailed)
+        }
+    }
+
+    func test_A8에서A9으로이동하는유효하지않은입력을하면_파싱에실패하여검증실패에러를던진다() throws {
+        let command = "A8->A9"
+
+        XCTAssertThrowsError(try CommandParser.parse(command)) { error in
+            XCTAssertEqual(error as? CommandParserError, .validationFailed)
+        }
+    }
+
+    func test_H8에서H9으로이동하는유효하지않은입력을하면_파싱에실패하여검증실패에러를던진다() throws {
+        let command = "H8->H9"
+
+        XCTAssertThrowsError(try CommandParser.parse(command)) { error in
+            XCTAssertEqual(error as? CommandParserError, .validationFailed)
+        }
+    }
+
+    func test_H8에서I8로이동하는유효하지않은입력을하면_파싱에실패하여검증실패에러를던진다() throws {
+        let command = "H8->I8"
+
+        XCTAssertThrowsError(try CommandParser.parse(command)) { error in
+            XCTAssertEqual(error as? CommandParserError, .validationFailed)
+        }
+    }
+}
