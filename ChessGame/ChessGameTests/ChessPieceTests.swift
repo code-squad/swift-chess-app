d4167ebf44fb21141dac61ba776a45785bd4213a
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-01 22:22:19 +0900

    테스트추가: 룩,비숍 테스트 추가

--- ChessGame/ChessGameTests/ChessPieceTests.swift
+++ ChessGame/ChessGameTests/ChessPieceTests.swift
@@ -39,4 +39,45 @@ final class ChessPieceTests: XCTestCase {
         )
         XCTAssertEqual(positions, expected)
     }
+    
+    func testAllMovementsOfBlackBishop() {
+        let board = ChessBoard.standardChessBoard()
+        
+        let positions = BishopPiece.availableMovingPositions(at: .init("C8")!, board: board)
+        let expected = Set(
+            [
+                "A6",
+                "E6", "F5", "G4", "H3"
+            ]
+                .map { Position($0)! }
+        )
+        XCTAssertEqual(positions, expected)
+    }
+    
+    func testAllMovementsOfWhiteRook() {
+        let board = ChessBoard.standardChessBoard()
+        
+        let positions = RookPiece.availableMovingPositions(at: .init("A1")!, board: board)
+        let expected = Set(
+            [
+                "A3", "A4", "A5", "A6", "A7", "A8",
+                "B1", "D1", "E1", "G1"
+            ]
+                .map { Position($0)! }
+        )
+        XCTAssertEqual(positions, expected)
+    }
+    func testAllMovementsOfBlackRook() {
+        let board = ChessBoard.standardChessBoard()
+        
+        let positions = RookPiece.availableMovingPositions(at: .init("A8")!, board: board)
+        let expected = Set(
+            [
+                "A6", "A5", "A4", "A3", "A2", "A1",
+                "B8", "D8", "E8", "G8"
+            ]
+                .map { Position($0)! }
+        )
+        XCTAssertEqual(positions, expected)
+    }
 }
