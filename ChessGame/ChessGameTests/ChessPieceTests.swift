c10ebf28f7fa113e60789955f67cd12bd340f283
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-01 19:47:12 +0900

    분리: 테스트스위트 파일 분리

---
+++ ChessGame/ChessGameTests/ChessPieceTests.swift
@@ -0,0 +1,26 @@
+//
+//  ChessPieceTesst.swift
+//  ChessGameTests
+//
+//  Created by Sunghyun Kim on 2022/10/01.
+//
+
+import XCTest
+@testable import ChessGame
+
+final class ChessPieceTests: XCTestCase {
+    
+    func testAllMovementsOfWhitePawn() {
+        let board = ChessBoard.standardChessBoard()
+        
+        let whitePawn = PawnPiece(teamColor: .white)
+        XCTAssertEqual(whitePawn.availableMovingPositions(at: .init("A2")!, board: board), [.init("A3")!])
+    }
+    
+    func testAllMovementsOfBlackPawn() {
+        let board = ChessBoard.standardChessBoard()
+        
+        let blackPawn = PawnPiece(teamColor: .black)
+        XCTAssertEqual(blackPawn.availableMovingPositions(at: .init("A7")!, board: board), [.init("A6")!])
+    }
+}
