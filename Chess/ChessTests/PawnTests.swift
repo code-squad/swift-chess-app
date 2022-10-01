b403a2c3790134dba2ee66c98d2891d28d857bf1
JAEYOUNGYUN <lastmore9@gmail.com>
2022-10-02 01:21:20 +0900

    Update PawnTests

--- Chess/ChessTests/PawnTests.swift
+++ Chess/ChessTests/PawnTests.swift
@@ -20,30 +20,29 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_emoji() {
         sut = Pawn(team: .white)
 
-        XCTAssertEqual(sut.emoji, "♙")
+        XCTAssertEqual(sut.emoji, .whitePawn)
     }
 
     func test_black_pawn_emoji() {
         sut = Pawn(team: .black)
 
-        XCTAssertEqual(sut.emoji, "♟")
+        XCTAssertEqual(sut.emoji, .blackPawn)
     }
 
     func test_white_pawn_movablePositions_from_left_edge() {
         sut = Pawn(team: .white)
 
-        let position = Position(fileIndex: 0, rankIndex: 7)
-        let expectedResult: [Position] = [
-            Position(fileIndex: 1, rankIndex: 7),
-            Position(fileIndex: 0, rankIndex: 6)
-        ]
-            .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
-            }
+        let position = Position(file: .A, rank: .eight)
+        let expectedResult = [
+            Position(file: .B, rank: .eight),
+            Position(file: .A, rank: .seven)
+        ].sorted {
+            $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
+        }
 
         let result = sut.movablePositions(from: position)
             .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
+                $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
             }
 
         XCTAssertEqual(result, expectedResult)
@@ -52,18 +51,17 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_movablePositions_from_right_edge() {
         sut = Pawn(team: .white)
 
-        let position = Position(fileIndex: 7, rankIndex: 7)
+        let position = Position(file: .H, rank: .eight)
         let expectedResult: [Position] = [
-            Position(fileIndex: 6, rankIndex: 7),
-            Position(fileIndex: 7, rankIndex: 6)
-        ]
-            .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
-            }
+            Position(file: .G, rank: .eight),
+            Position(file: .H, rank: .seven)
+        ].sorted {
+            $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
+        }
 
         let result = sut.movablePositions(from: position)
             .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
+                $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
             }
 
         XCTAssertEqual(result, expectedResult)
@@ -72,19 +70,18 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_movablePositions_from_normal() {
         sut = Pawn(team: .white)
 
-        let position = Position(fileIndex: 3, rankIndex: 7)
+        let position = Position(file: .D, rank: .eight)
         let expectedResult: [Position] = [
-            Position(fileIndex: 2, rankIndex: 7),
-            Position(fileIndex: 4, rankIndex: 7),
-            Position(fileIndex: 3, rankIndex: 6)
-        ]
-            .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
-            }
+            Position(file: .C, rank: .eight),
+            Position(file: .E, rank: .eight),
+            Position(file: .D, rank: .seven)
+        ].sorted {
+            $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
+        }
 
         let result = sut.movablePositions(from: position)
             .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
+                $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
             }
 
         XCTAssertEqual(result, expectedResult)
@@ -93,18 +90,17 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_movablePositions_from_left_edge() {
         sut = Pawn(team: .black)
 
-        let position = Position(fileIndex: 0, rankIndex: 2)
+        let position = Position(file: .A, rank: .three)
         let expectedResult: [Position] = [
-            Position(fileIndex: 1, rankIndex: 2),
-            Position(fileIndex: 0, rankIndex: 3)
-        ]
-            .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
-            }
+            Position(file: .B, rank: .three),
+            Position(file: .A, rank: .four)
+        ].sorted {
+            $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
+        }
 
         let result = sut.movablePositions(from: position)
             .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
+                $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
             }
 
         XCTAssertEqual(result, expectedResult)
@@ -113,18 +109,17 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_movablePositions_from_right_edge() {
         sut = Pawn(team: .black)
 
-        let position = Position(fileIndex: 7, rankIndex: 2)
+        let position = Position(file: .H, rank: .three)
         let expectedResult: [Position] = [
-            Position(fileIndex: 6, rankIndex: 2),
-            Position(fileIndex: 7, rankIndex: 3)
-        ]
-            .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
-            }
+            Position(file: .G, rank: .three),
+            Position(file: .H, rank: .four)
+        ].sorted {
+            $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
+        }
 
         let result = sut.movablePositions(from: position)
             .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
+                $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
             }
 
         XCTAssertEqual(result, expectedResult)
@@ -133,19 +128,18 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_movablePositions_from_normal() {
         sut = Pawn(team: .black)
 
-        let position = Position(fileIndex: 3, rankIndex: 2)
+        let position = Position(file: .D, rank: .three)
         let expectedResult: [Position] = [
-            Position(fileIndex: 2, rankIndex: 2),
-            Position(fileIndex: 4, rankIndex: 2),
-            Position(fileIndex: 3, rankIndex: 3)
-        ]
-            .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
-            }
+            Position(file: .C, rank: .three),
+            Position(file: .E, rank: .three),
+            Position(file: .D, rank: .four)
+        ].sorted {
+            $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
+        }
 
         let result = sut.movablePositions(from: position)
             .sorted {
-                $0.fileIndex < $1.fileIndex && $0.rankIndex < $1.rankIndex
+                $0.file.index < $1.file.index && $0.rank.index < $1.rank.index
             }
 
         XCTAssertEqual(result, expectedResult)
@@ -154,8 +148,8 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_canMove_one_left() {
         sut = Pawn(team: .white)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 2, rankIndex: 2)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .C, rank: .three)
 
         XCTAssertTrue(sut.canMove(from: from, to: to))
     }
@@ -163,8 +157,8 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_canMove_one_right() {
         sut = Pawn(team: .white)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 4, rankIndex: 2)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .E, rank: .three)
 
         XCTAssertTrue(sut.canMove(from: from, to: to))
     }
@@ -172,8 +166,8 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_canMove_one_forward() {
         sut = Pawn(team: .white)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 3, rankIndex: 1)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .D, rank: .two)
 
         XCTAssertTrue(sut.canMove(from: from, to: to))
     }
@@ -181,8 +175,8 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_cannotMove_one_backward() {
         sut = Pawn(team: .white)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 3, rankIndex: 3)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .D, rank: .four)
 
         XCTAssertFalse(sut.canMove(from: from, to: to))
     }
@@ -190,8 +184,8 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_cannotMove_two_left() {
         sut = Pawn(team: .white)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 1, rankIndex: 2)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .B, rank: .three)
 
         XCTAssertFalse(sut.canMove(from: from, to: to))
     }
@@ -199,8 +193,8 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_cannotMove_two_right() {
         sut = Pawn(team: .white)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 5, rankIndex: 2)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .F, rank: .three)
 
         XCTAssertFalse(sut.canMove(from: from, to: to))
     }
@@ -208,8 +202,8 @@ final class PawnTests: XCTestCase {
     func test_white_pawn_cannotMove_two_forward() {
         sut = Pawn(team: .white)
 
-        let from = Position(fileIndex: 3, rankIndex: 3)
-        let to = Position(fileIndex: 3, rankIndex: 1)
+        let from = Position(file: .D, rank: .four)
+        let to = Position(file: .D, rank: .two)
 
         XCTAssertFalse(sut.canMove(from: from, to: to))
     }
@@ -217,8 +211,8 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_canMove_one_left() {
         sut = Pawn(team: .black)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 2, rankIndex: 2)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .C, rank: .three)
 
         XCTAssertTrue(sut.canMove(from: from, to: to))
     }
@@ -226,8 +220,8 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_canMove_one_right() {
         sut = Pawn(team: .black)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 4, rankIndex: 2)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .E, rank: .three)
 
         XCTAssertTrue(sut.canMove(from: from, to: to))
     }
@@ -235,8 +229,8 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_canMove_one_forward() {
         sut = Pawn(team: .black)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 3, rankIndex: 3)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .D, rank: .four)
 
         XCTAssertTrue(sut.canMove(from: from, to: to))
     }
@@ -244,8 +238,8 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_cannotMove_one_backward() {
         sut = Pawn(team: .black)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 3, rankIndex: 1)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .D, rank: .two)
 
         XCTAssertFalse(sut.canMove(from: from, to: to))
     }
@@ -253,8 +247,8 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_cannotMove_two_left() {
         sut = Pawn(team: .black)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 1, rankIndex: 2)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .B, rank: .three)
 
         XCTAssertFalse(sut.canMove(from: from, to: to))
     }
@@ -262,8 +256,8 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_cannotMove_two_right() {
         sut = Pawn(team: .black)
 
-        let from = Position(fileIndex: 3, rankIndex: 2)
-        let to = Position(fileIndex: 5, rankIndex: 2)
+        let from = Position(file: .D, rank: .three)
+        let to = Position(file: .F, rank: .three)
 
         XCTAssertFalse(sut.canMove(from: from, to: to))
     }
@@ -271,8 +265,8 @@ final class PawnTests: XCTestCase {
     func test_black_pawn_cannotMove_two_forward() {
         sut = Pawn(team: .white)
 
-        let from = Position(fileIndex: 3, rankIndex: 3)
-        let to = Position(fileIndex: 3, rankIndex: 5)
+        let from = Position(file: .D, rank: .four)
+        let to = Position(file: .D, rank: .six)
 
         XCTAssertFalse(sut.canMove(from: from, to: to))
     }
