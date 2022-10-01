65f40d1b355c9ad424481804b92997671ca9957e
JAEYOUNGYUN <lastmore9@gmail.com>
2022-10-02 01:18:50 +0900

    Add Emoji.swift

---
+++ Chess/Chess/Emoji.swift
@@ -0,0 +1,13 @@
+//
+//  Emoji.swift
+//  Chess
+//
+//  Created by jaeyoung Yun on 2022/10/02.
+//
+
+import Foundation
+
+enum Emoji: String {
+    case whitePawn = "♙"
+    case blackPawn = "♟"
+}
