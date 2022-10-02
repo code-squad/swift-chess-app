ba93e28935ca045bafa5e3faeddaeb105643ac22
Geonhee Son <ryan.son1002@gmail.com>
2022-10-02 15:56:56 +0900

    refactor: `CommandParser`의 유효성 검사 기능을 `CommandValidator`로 이관

--- SwiftChess/SwiftChess/Sources/Board/Component/CommandParser.swift
+++ SwiftChess/SwiftChess/Sources/Board/Component/CommandParser.swift
@@ -21,7 +21,7 @@ enum CommandParser {
     static func parse(_ command: String) throws -> MoveCommand {
         let locations = command.split(separator: Self.Configuration.commandSeparator)
 
-        guard validate(locations) else {
+        guard CommandValidator.validate(locations) else {
             throw CommandParserError.validationFailed
         }
 
@@ -36,71 +36,6 @@ enum CommandParser {
             startPoint: startPointLocation,
             endPoint: endPointLocation
         )
-    }
-
-    /// ``CommandParser/Configuration/commandSeparator``를 기준으로 분리된 문자열들을 검증한다. 예) ["A1", "A2"]
-    private static func validate(_ locationStrings: [String.SubSequence]) -> Bool {
-        guard locationStrings.count == 2 else {
-            // TODO: 로그 CommandParserError.invalidCommand
-            return false
-        }
-
-        guard let startPoint = locationStrings.first else {
-            return false
-        }
-
-        guard let endPoint = locationStrings.last else {
-            return false
-        }
-
-        guard validateEach(startPoint) else {
-            return false
-        }
-
-        guard validateEach(endPoint) else {
-            return false
-        }
-
-        return true
-    }
-
-    /// 시작점 또는 도착점과 같이 하나의 문자열을 검증한다. 예) "A1"
-    private static func validateEach(_ locationString: Substring) -> Bool {
-        guard locationString.count == 2 else {
-            return false
-        }
-
-        guard let fileCandidate = locationString.first else {
-            return false
-        }
-
-        guard let rankCandidate = locationString.last else {
-            return false
-        }
-
-        let file = String(fileCandidate)
-        guard let rank = Int(String(rankCandidate)) else {
-            return false
-        }
-
-        guard let minimumFileAsciiValue = Board.Location.File.minimumCase?.asciiValue,
-              let maximumFileAsciiValue = Board.Location.File.maximumCase?.asciiValue else {
-            return false
-        }
-
-        guard (minimumFileAsciiValue...maximumFileAsciiValue).contains(file.asciiValue) else {
-            return false
-        }
-
-        guard let minimumRank = Board.Location.Rank.minimumRawValue,
-              let maximumRank = Board.Location.Rank.maximumRawValue else {
-            return false
-        }
-
-        guard (minimumRank...maximumRank).contains(rank) else {
-            return false
-        }
-        return true
     }
 
     /// `Substring` 타입의 문자열을 ``Board/Location`` 인스턴스로 바꾸어 반환한다.
---
+++ SwiftChess/SwiftChess/Sources/Board/Component/CommandValidator.swift
@@ -0,0 +1,74 @@
+//
+//  CommandValidator.swift
+//  SwiftChess
+//
+//  Created by Geonhee on 2022/10/02.
+//
+
+enum CommandValidator {
+
+    /// ``CommandParser/Configuration/commandSeparator``를 기준으로 분리된 문자열들을 검증한다. 예) ["A1", "A2"]
+    static func validate(_ locationStrings: [String.SubSequence]) -> Bool {
+        guard locationStrings.count == 2 else {
+            // TODO: 로그 CommandParserError.invalidCommand
+            return false
+        }
+
+        guard let startPoint = locationStrings.first else {
+            return false
+        }
+
+        guard let endPoint = locationStrings.last else {
+            return false
+        }
+
+        guard validateEach(startPoint) else {
+            return false
+        }
+
+        guard validateEach(endPoint) else {
+            return false
+        }
+
+        return true
+    }
+
+    /// 시작점 또는 도착점과 같이 하나의 문자열을 검증한다. 예) "A1"
+    private static func validateEach(_ locationString: Substring) -> Bool {
+        guard locationString.count == 2 else {
+            return false
+        }
+
+        guard let fileCandidate = locationString.first else {
+            return false
+        }
+
+        guard let rankCandidate = locationString.last else {
+            return false
+        }
+
+        let file = String(fileCandidate)
+        guard let rank = Int(String(rankCandidate)) else {
+            return false
+        }
+
+        guard let minimumFileAsciiValue = Board.Location.File.minimumCase?.asciiValue,
+              let maximumFileAsciiValue = Board.Location.File.maximumCase?.asciiValue else {
+            return false
+        }
+
+        guard (minimumFileAsciiValue...maximumFileAsciiValue).contains(file.asciiValue) else {
+            return false
+        }
+
+        guard let minimumRank = Board.Location.Rank.minimumRawValue,
+              let maximumRank = Board.Location.Rank.maximumRawValue else {
+            return false
+        }
+
+        guard (minimumRank...maximumRank).contains(rank) else {
+            return false
+        }
+        return true
+    }
+}
