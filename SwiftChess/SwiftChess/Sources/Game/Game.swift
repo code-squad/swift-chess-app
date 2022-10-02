af0994ae7e16ef69951ae492162232087ca24c5e
Geonhee Son <ryan.son1002@gmail.com>
2022-10-02 18:14:26 +0900

    refactor: 프로젝트 계층구조 정리 및 test placeholder 추가

---
+++ SwiftChess/SwiftChess/Sources/Game/Game.swift
@@ -0,0 +1,10 @@
+//
+//  Game.swift
+//  SwiftChess
+//
+//  Created by Geonhee on 2022/10/02.
+//
+
+final class Game {
+
+}
---
+++ SwiftChess/SwiftChessTests/Common/Extensions/StringExtensionsTests.swift
@@ -0,0 +1,12 @@
+//
+//  StringExtensionsTests.swift
+//  SwiftChessTests
+//
+//  Created by Geonhee on 2022/10/02.
+//
+
+import XCTest
+
+final class StringExtensionsTests: XCTestCase {
+
+}
