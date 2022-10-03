e5adb58e07cf9003b6779e6503a34682e3f1227a
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-03 20:40:32 +0900

    변경: 나이트 이동 경로에 기물이 있는지 확인

--- ChessGame/ChessGame/Chess/Chess Piece/KnightPiece.swift
+++ ChessGame/ChessGame/Chess/Chess Piece/KnightPiece.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+typealias Delta = Position
+
 struct KightPiece: ChessPieceProtocol {
     let teamColor: TeamColor
     static let score = 3
@@ -15,20 +17,56 @@ struct KightPiece: ChessPieceProtocol {
     }
     
     static func isMovementValid(origin: Position, destination: Position, board: ChessBoard) -> Bool {
-        let availableDeltas = [
-            (1, 2),
-            (2, 1),
-            (2, -1),
-            (-2, 1),
-            (-1, -2),
-            (-2, -1),
-            (1, -2),
-            (-1, 2)
-        ]
-        let delta = (
-            destination.file - origin.file,
-            destination.rank - origin.rank
-        )
-        return availableDeltas.contains { $0 == delta }
+        
+        let delta = origin - destination
+        let availableDeltas: [Delta]
+        let steppingPosition: Position
+        
+        // 지나가는 위치에 다른 기물이 막고 있지 않은지 확인한다.
+        if abs(delta.file) > abs(delta.rank) {
+            let fileIsPositive = delta.file > 0
+            steppingPosition = origin + Delta(file: fileIsPositive ? 1 : -1, rank: 0)
+            
+            availableDeltas = fileIsPositive ? [
+                Delta(file: 2, rank: 1),
+                Delta(file: 2, rank: -1)
+            ] : [
+                Delta(file: -2, rank: 1),
+                Delta(file: -2, rank: -1)
+            ]
+        } else {
+            let rankIsPositive = delta.rank > 0
+            steppingPosition = origin + Delta(file: 0, rank: rankIsPositive ? 1 : -1)
+            
+            availableDeltas = rankIsPositive ? [
+                Delta(file: 1, rank: 2),
+                Delta(file: -1, rank: 2)
+            ] : [
+                Delta(file: 1, rank: -2),
+                Delta(file: -1, rank: -2)
+            ]
+        }
+        
+        guard board[steppingPosition] == nil else { return false }
+        
+        return availableDeltas.contains(delta)
+    }
+}
+
+extension Delta {
+    static func -(lhs: Position, rhs: Position) -> Delta {
+        var lhs = lhs
+        lhs.file -= rhs.file
+        lhs.rank -= rhs.rank
+        return lhs
+    }
+}
+
+extension Position {
+    static func +(lhs: Position, rhs: Delta) -> Position {
+        var lhs = lhs
+        lhs.file += rhs.file
+        lhs.rank += rhs.rank
+        return lhs
     }
 }
