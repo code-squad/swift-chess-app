8d6e77e6cb417190dbc30ad0dadf31d9d49d636a
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-03 21:13:22 +0900

    리드미업데이트; 포지션 익스텐션 위치 변경

--- ChessGame/ChessGame/Chess/Position.swift
+++ ChessGame/ChessGame/Chess/Position.swift
@@ -13,3 +13,23 @@ struct Position: Equatable, Hashable {
     /// 가로 (row에 해당) 숫자 표기
     var rank: Int
 }
+
+typealias Delta = Position
+
+extension Delta {
+    static func -(lhs: Position, rhs: Position) -> Delta {
+        var lhs = lhs
+        lhs.file -= rhs.file
+        lhs.rank -= rhs.rank
+        return lhs
+    }
+}
+
+extension Position {
+    static func +(lhs: Position, rhs: Delta) -> Position {
+        var lhs = lhs
+        lhs.file += rhs.file
+        lhs.rank += rhs.rank
+        return lhs
+    }
+}
