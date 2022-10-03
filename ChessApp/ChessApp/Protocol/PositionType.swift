cab48fe814c8bc9000a7e0b67e5dd02c67b9370d
임승혁 <seunghyeok.lim@kurlycorp.com>
2022-10-03 18:13:10 +0900

    feature: PositionType protocol 구현 및 구현체 생성, 테스트 코드 추가

---
+++ ChessApp/ChessApp/Protocol/PositionType.swift
@@ -0,0 +1,25 @@
+//
+//  PositionType.swift
+//  ChessApp
+//
+//  Created by MK-AM16-010 on 2022/10/02.
+//
+
+import Foundation
+
+protocol PositionType {
+  var position: [String] { get }
+  var size: Int { get set }
+  
+  init?(position: [String], size: Int)
+  
+  func index(value: String) -> Int?
+}
+
+extension PositionType {
+  func index(value: String) -> Int? {
+    guard self.position.contains(value) else { return nil }
+    
+    return self.position.firstIndex(of: value)
+  }
+}
