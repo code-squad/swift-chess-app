8e4113c9e6b7521c25bd2f64bc7aed0039c068e3
JAEYOUNGYUN <lastmore9@gmail.com>
2022-10-05 05:01:40 +0900

    Update ChessBoard to set other pieces

--- Chess/Chess/ChessBoard.swift
+++ Chess/Chess/ChessBoard.swift
@@ -27,19 +27,59 @@ class ChessBoard: Board {
             count: boardSize
         )
 
-        scores = [.white: Score(point: 8), .black: Score(point: 8)]
+        scores = [:]
         reset()
     }
 
     func reset() {
-        scores = [.white: Score(point: 8), .black: Score(point: 8)]
+        scores = [.white: Score(point: 0), .black: Score(point: 0)]
 
         for i in 0..<boardSize {
             for j in 0..<boardSize {
-                if i == 1 {
-                    boards[i][j] = Pawn(team: .black)
+                if i == 0 {
+                    if j == 0 || j == 7 {
+                        let blackRook = Rook(team: .black)
+                        boards[i][j] = blackRook
+                        scores[.black] = (scores[.black] ?? Score(point: 0)) + blackRook.score
+                    } else if j == 1 || j == 6 {
+                        let blackKnight = Knight(team: .black)
+                        boards[i][j] = blackKnight
+                        scores[.black] = (scores[.black] ?? Score(point: 0)) + blackKnight.score
+                    } else if j == 2 || j == 5 {
+                        let blackBishop = Bishop(team: .black)
+                        boards[i][j] = blackBishop
+                        scores[.black] = (scores[.black] ?? Score(point: 0)) + blackBishop.score
+                    } else if j == 4 {
+                        let blackQueen = Queen(team: .black)
+                        boards[i][j] = blackQueen
+                        scores[.black] = (scores[.black] ?? Score(point: 0)) + blackQueen.score
+                    }
+                } else if i == 1 {
+                    let blackPawn = Pawn(team: .black)
+                    boards[i][j] = blackPawn
+                    scores[.black] = (scores[.black] ?? Score(point: 0)) + blackPawn.score
                 } else if i == 6 {
-                    boards[i][j] = Pawn(team: .white)
+                    let whitePawn = Pawn(team: .white)
+                    boards[i][j] = whitePawn
+                    scores[.white] = (scores[.white] ?? Score(point: 0)) + whitePawn.score
+                } else if i == 7 {
+                    if j == 0 || j == 7 {
+                        let whiteRook = Rook(team: .white)
+                        boards[i][j] = whiteRook
+                        scores[.white] = (scores[.white] ?? Score(point: 0)) + whiteRook.score
+                    } else if j == 1 || j == 6 {
+                        let whiteKnight = Knight(team: .white)
+                        boards[i][j] = whiteKnight
+                        scores[.white] = (scores[.white] ?? Score(point: 0)) + whiteKnight.score
+                    } else if j == 2 || j == 5 {
+                        let whiteBishop = Bishop(team: .white)
+                        boards[i][j] = whiteBishop
+                        scores[.white] = (scores[.white] ?? Score(point: 0)) + whiteBishop.score
+                    } else if j == 4 {
+                        let whiteQueen = Queen(team: .white)
+                        boards[i][j] = whiteQueen
+                        scores[.white] = (scores[.white] ?? Score(point: 0)) + whiteQueen.score
+                    }
                 } else {
                     boards[i][j] = nil
                 }
