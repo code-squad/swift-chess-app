5ce596062ec0fee8c4f59a1e1194254238edbb1d
JAEYOUNGYUN <lastmore9@gmail.com>
2022-10-05 04:57:17 +0900

    Add emoji in Emoji

--- Chess/Chess/Emoji.swift
+++ Chess/Chess/Emoji.swift
@@ -10,4 +10,12 @@ import Foundation
 enum Emoji: String {
     case whitePawn = "♙"
     case blackPawn = "♟"
+    case whiteBishop = "♗"
+    case blackBishop = "♝"
+    case whiteRook = "♖"
+    case blackRook = "♜"
+    case whiteQueen = "♕"
+    case blackQueen = "♛"
+    case whiteKnight = "♘"
+    case blackKnight = "♞"
 }
