d37ffed182e4cd4f9a99303d8982b2567404575b
김승언[iOS개발] <seungeon.kim_12@snowcorp.com>
2022-10-05 05:10:41 +0900

    test: ChessBoard 모델 변경으로인한 테스트 추가

--- swift-chess-app/swift-chess-appTests/ChessGameTest.swift
+++ swift-chess-app/swift-chess-appTests/ChessGameTest.swift
@@ -16,8 +16,8 @@ class ChessGameTest: XCTestCase {
         chessGame.chessBoard.createPawns(color: .black, positions: ChessGame.startPositionOfBlackPawns())
         chessGame.chessBoard.createPawns(color: .white, positions: ChessGame.startPositionOfWhitePawns())
         //when
-        chessGame.chessBoard.myPawns(color: .white).forEach({
-            chessGame.chessBoard.removePawn(color: .white, pawn: $0)
+        chessGame.chessBoard.myPieces(color: .white).forEach({
+            chessGame.chessBoard.killChessPiece($0)
         })
         let shouldContinueGame = chessGame.checkContinueGame()
         //then
@@ -29,34 +29,34 @@ class ChessGameTest: XCTestCase {
         chessGame.chessBoard.createPawns(color: .black, positions: ChessGame.startPositionOfBlackPawns())
         chessGame.chessBoard.createPawns(color: .white, positions: ChessGame.startPositionOfWhitePawns())
         //when
-        chessGame.chessBoard.myPawns(color: .white).enumerated().forEach({
+        chessGame.chessBoard.myPieces(color: .white).enumerated().forEach({
             if $0 != 0 {
-                chessGame.chessBoard.removePawn(color: .white, pawn: $1)
+                chessGame.chessBoard.killChessPiece($1)
             }
         })
         let shouldContinueGame = chessGame.checkContinueGame()
         //then
         XCTAssertTrue(shouldContinueGame)
     }
-    // // MARK: - 백색은 큰 rank에서 더 작은 rank로 움직일 수 있고, 흑색은 작은 rank에서 더 큰 rank로 움직일 수 있다.
+    
     func test_이동_가능한_경로_확인_흰색_A7_To_A6() {
         //given
         chessGame.chessBoard.createPawns(color: .white, positions: ChessGame.startPositionOfWhitePawns())
-        let targetPawn = chessGame.chessBoard.findPawn(color: .white, position: Position(file: .a, rank: .seven))!
-        let toPosition = Position(file: targetPawn.position.file, rank: Rank(rawValue: targetPawn.position.rank.rawValue - 1)!)
+        let targetPiece = chessGame.chessBoard.myPieces(color: .white).first(where: { $0.position ==  Position(file: .a, rank: .seven) })!
+        let toPosition = Position(file: targetPiece.position.file, rank: Rank(rawValue: targetPiece.position.rank.rawValue - 1)!)
         //when
-        let canMove = chessGame.checkValidatePosition(player: .white, from: targetPawn.position, to: toPosition)
+        let canNotMove = chessGame.checkValidatePosition(playerColor: .black, from: targetPiece.position, to: toPosition)
         //then
-        XCTAssertTrue(canMove)
+        XCTAssertFalse(canNotMove)
     }
     
     func test_이동_가능한_경로_확인_흰색_A7_To_A5() {
         //given
         chessGame.chessBoard.createPawns(color: .white, positions: ChessGame.startPositionOfWhitePawns())
-        let targetPawn = chessGame.chessBoard.findPawn(color: .white, position: Position(file: .a, rank: .seven))!
-        let toPosition = Position(file: targetPawn.position.file, rank: Rank(rawValue: targetPawn.position.rank.rawValue - 2)!)
+        let targetPiece = chessGame.chessBoard.myPieces(color: .white).first(where: { $0.position ==  Position(file: .a, rank: .seven) })!
+        let toPosition = Position(file: targetPiece.position.file, rank: Rank(rawValue: targetPiece.position.rank.rawValue - 2)!)
         //when
-        let canNotMove = chessGame.checkValidatePosition(player: .white, from: targetPawn.position, to: toPosition)
+        let canNotMove = chessGame.checkValidatePosition(playerColor: .white, from: targetPiece.position, to: toPosition)
         //then
         XCTAssertFalse(canNotMove)
     }
@@ -64,10 +64,10 @@ class ChessGameTest: XCTestCase {
     func test_이동_가능한_경로_확인_흰색_A7_To_A8() {
         //given
         chessGame.chessBoard.createPawns(color: .white, positions: ChessGame.startPositionOfWhitePawns())
-        let targetPawn = chessGame.chessBoard.findPawn(color: .white, position: Position(file: .a, rank: .seven))!
-        let toPosition = Position(file: targetPawn.position.file, rank: Rank(rawValue: targetPawn.position.rank.rawValue + 1)!)
+        let targetPiece = chessGame.chessBoard.myPieces(color: .white).first(where: { $0.position ==  Position(file: .a, rank: .seven) })!
+        let toPosition = Position(file: targetPiece.position.file, rank: Rank(rawValue: targetPiece.position.rank.rawValue + 1)!)
         //when
-        let canNotMove = chessGame.checkValidatePosition(player: .white, from: targetPawn.position, to: toPosition)
+        let canNotMove = chessGame.checkValidatePosition(playerColor: .white, from: targetPiece.position, to: toPosition)
         //then
         XCTAssertFalse(canNotMove)
     }
@@ -76,21 +76,21 @@ class ChessGameTest: XCTestCase {
     func test_이동_가능한_경로_확인_검은색_A2_To_A3() {
         //given
         chessGame.chessBoard.createPawns(color: .black, positions: ChessGame.startPositionOfBlackPawns())
-        let targetPawn = chessGame.chessBoard.findPawn(color: .black, position: Position(file: .a, rank: .two))!
-        let toPosition = Position(file: targetPawn.position.file, rank: Rank(rawValue: targetPawn.position.rank.rawValue + 1)!)
+        let targetPiece = chessGame.chessBoard.myPieces(color: .black).first(where: { $0.position ==  Position(file: .a, rank: .two) })!
+        let toPosition = Position(file: targetPiece.position.file, rank: Rank(rawValue: targetPiece.position.rank.rawValue + 1)!)
         //when
-        let canMove = chessGame.checkValidatePosition(player: .black, from: targetPawn.position, to: toPosition)
+        let canNotMove = chessGame.checkValidatePosition(playerColor: .black, from: targetPiece.position, to: toPosition)
         //then
-        XCTAssertTrue(canMove)
+        XCTAssertFalse(canNotMove)
     }
     
     func test_이동_가능한_경로_확인_검은색_A2_To_A1() {
         //given
         chessGame.chessBoard.createPawns(color: .black, positions: ChessGame.startPositionOfBlackPawns())
-        let targetPawn = chessGame.chessBoard.findPawn(color: .black, position: Position(file: .a, rank: .two))!
-        let toPosition = Position(file: targetPawn.position.file, rank: Rank(rawValue: targetPawn.position.rank.rawValue - 1)!)
+        let targetPiece = chessGame.chessBoard.myPieces(color: .black).first(where: { $0.position ==  Position(file: .a, rank: .two) })!
+        let toPosition = Position(file: targetPiece.position.file, rank: Rank(rawValue: targetPiece.position.rank.rawValue - 1)!)
         //when
-        let canNotMove = chessGame.checkValidatePosition(player: .black, from: targetPawn.position, to: toPosition)
+        let canNotMove = chessGame.checkValidatePosition(playerColor: .black, from: targetPiece.position, to: toPosition)
         //then
         XCTAssertFalse(canNotMove)
     }
@@ -98,13 +98,12 @@ class ChessGameTest: XCTestCase {
     func test_이동_가능한_경로_확인_검은색_A2_To_A3_To_A1() {
         //given
         chessGame.chessBoard.createPawns(color: .black, positions: ChessGame.startPositionOfBlackPawns())
-        let targetPawn = chessGame.chessBoard.findPawn(color: .black, position: Position(file: .a, rank: .two))!
-        let toPosition1 = Position(file: targetPawn.position.file, rank: Rank(rawValue: targetPawn.position.rank.rawValue + 1)!)
-        chessGame.chessBoard.movePawn(color: .black, from: targetPawn.position, to: toPosition1)
+        let targetPiece = chessGame.chessBoard.myPieces(color: .black).first(where: { $0.position ==  Position(file: .a, rank: .two) })!
+        let toPosition1 = Position(file: targetPiece.position.file, rank: Rank(rawValue: targetPiece.position.rank.rawValue + 1)!)
+        chessGame.chessBoard.movePiece(color: .black, from: targetPiece.position, to: toPosition1)
         let toPosition2 = Position(file: toPosition1.file, rank: Rank(rawValue: toPosition1.rank.rawValue - 2)!)
         //when
-        let canNotMove = chessGame.checkValidatePosition(player: .black, from: toPosition1, to: toPosition2)
-        
+        let canNotMove = chessGame.checkValidatePosition(playerColor: .black, from: toPosition1, to: toPosition2)
         //then
         XCTAssertFalse(canNotMove)
     }
@@ -112,24 +111,25 @@ class ChessGameTest: XCTestCase {
     func test_스코어_확인() {
         //given
         chessGame.chessBoard.createPawns(color: .black, positions: ChessGame.startPositionOfBlackPawns())
-        let initScore = chessGame.chessBoard.myPawns(color: .black).count
-        let removePawn1 = chessGame.chessBoard.myPawns(color: .black).first!
-        let removePawn2 = chessGame.chessBoard.myPawns(color: .black).last!
+        chessGame.chessBoard.createKnights(color: .black, positions: ChessGame.startPositionOfBlackKnights())
+        let initScore = chessGame.chessBoard.myPieces(color: .black).map({ $0.point }).reduce(0, { $0 + $1 })
+        let removePawn = chessGame.chessBoard.myPieces(color: .black).filter({ $0 is Pawn }).first!
+        let removeKnight = chessGame.chessBoard.myPieces(color: .black).filter({ $0 is Knight }).first!
         //when
-        chessGame.chessBoard.removePawn(color: .black, pawn: removePawn1)
-        chessGame.chessBoard.removePawn(color: .black, pawn: removePawn2)
-        let score = initScore - 2
+        chessGame.chessBoard.killChessPiece(removePawn)
+        chessGame.chessBoard.killChessPiece(removeKnight)
+        let score = initScore - Dimension.Point.pawn - Dimension.Point.knight
         //then
         
-        XCTAssertEqual(score, 6)
+        XCTAssertEqual(score, chessGame.score())
     }
     
     func test_플레이어_턴_교환() {
         //given
-        let prevTurnPlayer = chessGame.currentPalyer
+        let prevTurnPlayer = chessGame.currentPalyerColor
         //when
         chessGame.changeTurnPalyer()
-        let nowTurnPlayer = chessGame.currentPalyer
+        let nowTurnPlayer = chessGame.currentPalyerColor
         //then
          XCTAssertNotEqual(prevTurnPlayer, nowTurnPlayer)
     }
