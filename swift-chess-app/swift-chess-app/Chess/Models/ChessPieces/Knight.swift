3b8fd154ef4e9747591e3a3421a9900c174a135b
김승언[iOS개발] <seungeon.kim_12@snowcorp.com>
2022-10-05 07:06:50 +0900

    feat: 체스 말 구조체 분리

---
+++ swift-chess-app/swift-chess-app/Chess/Models/ChessPieces/Knight.swift
@@ -0,0 +1,29 @@
+//
+//  Knight.swift
+//  swift-chess-app
+//
+//  Created by USER on 2022/10/04.
+//
+
+import Foundation
+
+struct Knight: Equatable, ChessPiecable, ChessPieceMovableChecker {
+    private(set) var color: ChessPieceColor
+    var position: Position
+    
+    var point: Int {
+        return Dimension.Point.knight
+    }
+
+    // MARK: 나이트는 점프로만 이동이 가능 (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2)
+    var movableDirection: [ChessMovableDirection] {
+        return [.jumpToOneClock, .jumpToTwoClock,
+                .jumpToFourClock, .jumpToFiveClock,
+                .jumpToSevenClock, .jumpToEightClock,
+                .jumpToTenClock, .jumpToElevenClock]
+    }
+    
+    var description: String {
+        return color == .black ? "♞" : "♘"
+    }
+}
