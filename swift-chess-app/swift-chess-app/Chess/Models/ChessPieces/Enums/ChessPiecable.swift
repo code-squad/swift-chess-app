1e62fb7903048b0d9d1ee1c14cb707efb75b9702
김승언[iOS개발] <seungeon.kim_12@snowcorp.com>
2022-10-05 07:04:33 +0900

    feat: 체스 말 공통 프로토콜 추가

---
+++ swift-chess-app/swift-chess-app/Chess/Models/ChessPieces/Enums/ChessPiecable.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  swift-chess-app
+//
+//  Created by USER on 2022/10/04.
+//
+
+import Foundation
+
+protocol ChessPiecable: CustomStringConvertible {
+    var color: ChessPieceColor { get }
+    var position: Position { get set }
+    var point: Int { get }
+    mutating func move(to position: Position)
+    func movablePositions() -> [Position]
+}
+
+extension ChessPiecable {
+    mutating func move(to position: Position) {
+        self.position = position
+    }
+}
---
+++ swift-chess-app/swift-chess-app/Chess/Models/ChessPieces/Enums/ChessPieceColor.swift
@@ -0,0 +1,12 @@
+//
+//  ChessPieceColor.swift
+//  swift-chess-app
+//
+//  Created by USER on 2022/10/04.
+//
+
+import Foundation
+
+enum ChessPieceColor: String {
+    case black, white
+}
