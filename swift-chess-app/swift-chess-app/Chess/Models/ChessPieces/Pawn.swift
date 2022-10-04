854ab9eb1abaa2b61e219ce9c01a6c0dc672ff74
김승언[iOS개발] <seungeon.kim_12@snowcorp.com>
2022-10-05 01:09:46 +0900

    feat: 체스 말 구조체 분리

---
+++ swift-chess-app/swift-chess-app/Chess/Models/ChessPieces/Pawn.swift
@@ -0,0 +1,32 @@
+//
+//  Pawn.swift
+//  swift-chess-app
+//
+//  Created by USER on 2022/09/26.
+//
+
+import Foundation
+
+struct Pawn: Equatable, ChessPiecable, ChessPieceMovableChecker {
+    private(set) var color: ChessPieceColor
+    var position: Position
+    
+    var point: Int {
+        return Dimension.Point.pawn
+    }
+    
+    // MARK: 백색은 더 작은 rank로 움직일 수 있고, 흑색은 더 큰 rank로 움직일 수 있다.
+    var movableDirection: [ChessMovableDirection] {
+        return color == .black ? [.go] : [.back]
+    }
+    
+    var description: String {
+        return color == .black ? "♟" : "♙"
+    }
+    
+    func movablePositions() -> [Position] {
+        guard let movablePosition = self.movablePositions(self.position).first else { return [] }
+        return [movablePosition]
+    }
+}
+
---
+++ swift-chess-app/swift-chess-app/Chess/Models/ChessPieces/Queen.swift
@@ -0,0 +1,27 @@
+//
+//  Queen.swift
+//  swift-chess-app
+//
+//  Created by USER on 2022/10/04.
+//
+
+import Foundation
+
+struct Queen: Equatable, ChessPiecable, ChessPieceMovableChecker {
+    private(set) var color: ChessPieceColor
+    var position: Position
+    
+    var point: Int {
+        return Dimension.Point.queen
+    }
+    
+    // MARK: 퀸은 대각선 및 직선으로만 이동이 가능 (-1, -1) / (-1, +1) / (+1. -1) / (+1, +1) / (0, 1) / (0, -1) / (1. 0) / (-1, 0)
+    var movableDirection: [ChessMovableDirection] {
+        return [.go, .back, .left, .right,
+                .leftGo, .leftBack, .rightGo, .rightBack]
+    }
+    
+    var description: String {
+        return color == .black ? "♛" : "♕"
+    }
+}
