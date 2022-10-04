e1eb0e4dfbc375bcb85d12ae8a44f239b1a0b95b
김승언[iOS개발] <seungeon.kim_12@snowcorp.com>
2022-10-05 07:08:50 +0900

    feat: 모델 변경으로 인한 로직 수정

--- swift-chess-app/swift-chess-app/Chess/Models/ChessBoard.swift
+++ swift-chess-app/swift-chess-app/Chess/Models/ChessBoard.swift
@@ -8,92 +8,93 @@
 import Foundation
 
 class ChessBoard {
-    private var blackPawns: [Pawn] = []
-    private var whitePawns: [Pawn] = []
+    private var chessPieces: ChessPieces = ChessPieces()
     
-    func createPawns(color: Pawn.Color, positions: [Position]) {
-        let pawns = positions.map({ Pawn(color: color, position: $0) })
-        
-        if color == .black {
-            self.blackPawns = pawns
-        } else {
-            self.whitePawns = pawns
-        }
-    }
-    
-    func myPawns(color: Pawn.Color) -> [Pawn] {
-        let pawns = color == .black ? self.blackPawns : self.whitePawns
-        return pawns
+    func myPieces(color: ChessPieceColor) -> [ChessPiecable] {
+        return chessPieces.all().filter({ $0.color == color })
     }
     
     @discardableResult
-    func movePawn(color: Pawn.Color, from: Position, to: Position) -> Bool {
-        guard var targetPawn = findPawn(color: color, position: from),
-              let _ = removePawn(color: color, pawn: targetPawn) else { return false }
-        targetPawn.move(to)
-        self.addPawn(color: color, pawn: targetPawn)
+    func movePiece(color: ChessPieceColor, from: Position, to: Position) -> Bool {
+        guard var piece = chessPieces.find(color: color, position: from) else { return false }
+        piece.move(to: to)
         return true
     }
     
-    @discardableResult
-    func removePawn(color: Pawn.Color , pawn: Pawn) -> Pawn? {
-        if color == .black {
-            guard let index = blackPawns.firstIndex(where: { $0 == pawn }) else { return nil }
-            return blackPawns.remove(at: index)
-        } else {
-            guard let index = whitePawns.firstIndex(where: { $0 == pawn }) else { return nil }
-            return whitePawns.remove(at: index)
-        }
+    func movablePositions(color: ChessPieceColor, position: Position) -> [Position] {
+        guard let piece = chessPieces.find(color: color, position: position) else { return [] }
+        return piece.movablePositions()
     }
     
-    private func addPawn(color: Pawn.Color , pawn: Pawn) {
-        if color == .black {
-            self.blackPawns.append(pawn)
-        } else {
-            self.whitePawns.append(pawn)
-        }
+    func checkHitPiece(color: ChessPieceColor, position: Position) -> ChessPiecable? {
+        return chessPieces.find(color: color, position: position)
     }
     
-    func findPawn(color: Pawn.Color , position: Position) -> Pawn? {
-        let pawns = color == .black ? self.blackPawns : self.whitePawns
-        return pawns.first(where: { $0.position == position })
+    func killChessPiece(_ piece: ChessPiecable) {
+        chessPieces.remove(piece)
     }
-    
-    func checkHitPawn(color: Pawn.Color, position: Position) -> Pawn? {
-        let targetPawns = color == .black ? self.whitePawns : self.blackPawns
-        return targetPawns.first(where: { $0.position == position })
-    }
-    
+
+
     func display() {
-        var matrix: [[Pawn?]]
-        let rank: [Pawn?] = Array(repeating: nil, count: 8)
+        var matrix: [[ChessPiecable?]]
+        let rank: [ChessPiecable?] = Array(repeating: nil, count: 8)
         matrix = Array(repeating: rank, count: 8)
-        
-        self.whitePawns.forEach({ whitePawn in
-            matrix[whitePawn.position.rank.rawValue - 1][whitePawn.position.file.rawValue - 1] = whitePawn
+
+        self.chessPieces.all().forEach({ piece in
+            matrix[piece.position.rank.rawValue - 1][piece.position.file.rawValue - 1] = piece
         })
-        
-        self.blackPawns.forEach({ blackPawn in
-            matrix[blackPawn.position.rank.rawValue - 1][blackPawn.position.file.rawValue - 1] = blackPawn
-        })
-        
+
         var string = ""
         string += File.allCases.map{ $0.invertedTypeString }.reduce(" ", { $0 + $1 })
         string += "\n"
-        
+
         Rank.allCases.forEach({ rank in
             string += "\(rank.invertedTypeString)"
             string +=  matrix[rank.rawValue - 1]
                 .map({
-                    if let pawn = $0 { return pawn.color == .black ? "♟" : "♙" }
+                    if let piece = $0 { return piece.description }
                     else { return "." }
                 })
                 .reduce("", { $0 + $1 })
             string += "\n"
         })
-        
+
         string += File.allCases.map{ $0.invertedTypeString }.reduce(" ", { $0 + $1 })
-        
+
         print(string)
+    }
+}
+
+
+// MARK: - Piece Maker
+extension ChessBoard {
+    func createPawns(color: ChessPieceColor, positions: [Position]) {
+        positions.forEach {
+            self.chessPieces.add(Pawn(color: color, position: $0))
+        }
+    }
+    
+    func createBishops(color: ChessPieceColor, positions: [Position]) {
+        positions.forEach {
+            self.chessPieces.add(Bishop(color: color, position: $0))
+        }
+    }
+    
+    func createRooks(color: ChessPieceColor, positions: [Position]) {
+        positions.forEach {
+            self.chessPieces.add(Rook(color: color, position: $0))
+        }
+    }
+    
+    func createQueens(color: ChessPieceColor, positions: [Position]) {
+        positions.forEach {
+            self.chessPieces.add(Queen(color: color, position: $0))
+        }
+    }
+    
+    func createKnights(color: ChessPieceColor, positions: [Position]) {
+        positions.forEach {
+            self.chessPieces.add(Knight(color: color, position: $0))
+        }
     }
 }
--- swift-chess-app/swift-chess-app/Chess/Models/ChessGame.swift
+++ swift-chess-app/swift-chess-app/Chess/Models/ChessGame.swift
@@ -8,16 +8,19 @@
 import Foundation
 
 class ChessGame {
-    typealias Player = Pawn.Color
+    typealias PlayerColor = ChessPieceColor
     
     let chessBoard = ChessBoard()
-    var currentPalyer: Player = .black
+    var currentPalyerColor: PlayerColor = .black
     
-    func start(inputString: String) {
-        chessBoard.createPawns(color: .black, positions: Self.startPositionOfBlackPawns())
-        chessBoard.createPawns(color: .white, positions: Self.startPositionOfWhitePawns())
+    func start() {
+        // 생성
+        createPieces()
         
+        // 게임 진행
         while checkContinueGame() {
+            let inputString = ""
+
             guard let command = Command.make(inputString: inputString),
                   let from = command.fromPosition(),
                   let to = command.toPosition()
@@ -25,52 +28,45 @@ class ChessGame {
                 printError(.invalidFormat)
                 continue
             }
-            
-            guard checkValidatePosition(player: currentPalyer, from: from, to: to)
+
+            guard checkValidatePosition(playerColor: currentPalyerColor, from: from, to: to)
             else {
                 printError(.invalidPosition)
                 continue
             }
-            
-            guard chessBoard.movePawn(color: currentPalyer, from: from, to: to)
+
+            guard chessBoard.movePiece(color: currentPalyerColor, from: from, to: to)
             else {
                 printError(.notFindPawn)
                 continue
             }
-                
-            if let removePawn = chessBoard.checkHitPawn(color: currentPalyer, position: to) {
-                let otherPlayer: Player = currentPalyer == .black ? .white : .black
-                chessBoard.removePawn(color: otherPlayer, pawn: removePawn)
+
+            if let removeTarget = chessBoard.checkHitPiece(color: currentPalyerColor, position: to) {
+                chessBoard.killChessPiece(removeTarget)
             }
-                
+
+            // 출력
             chessBoard.display()
             changeTurnPalyer()
         }
     }
     
+    
     func changeTurnPalyer() {
-        let turnPalyer: Player = currentPalyer == .black ? .white : .black
-        self.currentPalyer = turnPalyer
+        let turnPalyer: PlayerColor = currentPalyerColor == .black ? .white : .black
+        self.currentPalyerColor = turnPalyer
     }
     
-    // MARK: - 백색은 큰 rank에서 더 작은 rank로 움직일 수 있고, 흑색은 작은 rank에서 더 큰 rank로 움직일 수 있다.
-    func checkValidatePosition(player: Player, from: Position, to: Position) -> Bool {
-        guard chessBoard.findPawn(color: player, position: to) == nil else { return false }
-        
-        let canDirectionToInt: Int = player == .white ? 1 : -1
-        if ((from.file.rawValue == to.file.rawValue) && ((from.rank.rawValue - to.rank.rawValue) == canDirectionToInt))
-            || ((abs(from.file.rawValue - to.file.rawValue) == 1) && (from.rank.rawValue == to.rank.rawValue)) {
-            return true
-        }
-        
-        return false
+    func checkValidatePosition(playerColor: PlayerColor, from: Position, to: Position) -> Bool {
+        let movablePosition = chessBoard.movablePositions(color: playerColor, position: from )
+        return movablePosition.first(where: { $0 == to }) != nil
     }
     
     func checkContinueGame() -> Bool {
         var shouldContinue = true
         
-        [Player.black, Player.white].forEach({
-            if chessBoard.myPawns(color: $0).count == 0 {
+        [PlayerColor.black, PlayerColor.white].forEach({
+            if chessBoard.myPieces(color: $0).count == 0 {
                 shouldContinue = false
             }
         })
@@ -82,12 +78,52 @@ class ChessGame {
     }
     
     func score() -> Int {
-        let score = chessBoard.myPawns(color: self.currentPalyer).count
-        return score
+        return chessBoard
+            .myPieces(color: self.currentPalyerColor)
+            .map({ $0.point })
+            .reduce(0, { $0 + $1 })
     }
     
 }
 
+// MARK: - Create Pieces
+extension ChessGame {
+    private func createPieces() {
+        createPawns()
+        createRooks()
+        createQueens()
+        createBishops()
+        createKnights()
+    }
+    
+    private func createPawns() {
+        chessBoard.createPawns(color: .black, positions: Self.startPositionOfBlackPawns())
+        chessBoard.createPawns(color: .white, positions: Self.startPositionOfWhitePawns())
+    }
+    
+    private func createBishops() {
+        chessBoard.createBishops(color: .black, positions: Self.startPositionOfBlackBishops())
+        chessBoard.createBishops(color: .white, positions: Self.startPositionOfWhiteBishops())
+    }
+    
+    private func createRooks() {
+        chessBoard.createRooks(color: .black, positions: Self.startPositionOfBlackRooks())
+        chessBoard.createRooks(color: .white, positions: Self.startPositionOfWhiteRooks())
+    }
+    
+    private func createQueens() {
+        chessBoard.createQueens(color: .black, positions: Self.startPositionOfBlackQueens())
+        chessBoard.createQueens(color: .white, positions: Self.startPositionOfWhiteQueens())
+    }
+    
+    private func createKnights() {
+        chessBoard.createKnights(color: .black, positions: Self.startPositionOfBlackKnights())
+        chessBoard.createKnights(color: .white, positions: Self.startPositionOfWhiteKnights())
+    }
+}
+
+
+// MARK: - Statics
 extension ChessGame {
     // MARK: - 초기 생성 위치는 흑색은 2-rank 또는 백색 7-rank에만 가능하다.
     static func startPositionOfBlackPawns() -> [Position] {
@@ -96,6 +132,48 @@ extension ChessGame {
     
     static func startPositionOfWhitePawns() -> [Position] {
         return File.allCases.map({ Position(file: $0, rank: .seven) })
+    }
+    
+    // MARK: - 생성 위치는 흑색은 C-1 과 F-1 에만 가능하고, 백색은 C-8 과 F-8 에만 가능하다.
+    static func startPositionOfBlackBishops() -> [Position] {
+        return [Position(file: .c, rank: .one),
+                Position(file: .f, rank: .one)]
+    }
+    
+    static func startPositionOfWhiteBishops() -> [Position] {
+        return [Position(file: .c, rank: .eight),
+                Position(file: .f, rank: .eight)]
+    }
+    
+    // MARK: - 생성 위치는 흑색은 A-1 과 H-1 에만 가능하고, 백색은 A-8 과 H-8 에만 가능하다.
+    static func startPositionOfBlackRooks() -> [Position] {
+        return [Position(file: .a, rank: .one),
+                Position(file: .h, rank: .one)]
+    }
+    
+    static func startPositionOfWhiteRooks() -> [Position] {
+        return [Position(file: .a, rank: .eight),
+                Position(file: .h, rank: .eight)]
+    }
+    
+    // MARK: - 생성 위치는 흑색은 E-1에만 가능하고, 백색은 E-8 에만 가능하다.
+    static func startPositionOfBlackQueens() -> [Position] {
+        return [Position(file: .e, rank: .one)]
+    }
+    
+    static func startPositionOfWhiteQueens() -> [Position] {
+        return [Position(file: .e, rank: .eight)]
+    }
+    
+    // MARK: - 생성 위치는 흑색은 B-1 과 G-1 에만 가능하고, 백색은 B-8 과 G-8 에만 가능하다.
+    static func startPositionOfBlackKnights() -> [Position] {
+        return [Position(file: .b, rank: .one),
+                Position(file: .g, rank: .one)]
+    }
+    
+    static func startPositionOfWhiteKnights() -> [Position] {
+        return [Position(file: .b, rank: .eight),
+                Position(file: .g, rank: .eight)]
     }
 }
 
