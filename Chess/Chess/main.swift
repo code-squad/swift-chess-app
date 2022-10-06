6dc337d5055e3d9597fde3088ccb693e5ed66f65
JAEYOUNGYUN <lastmore9@gmail.com>
2022-10-06 21:41:13 +0900

    Init project

---
+++ Chess/Chess/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Chess
+//
+//  Created by jaeyoung Yun on 2022/10/06.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+class Main {
+
+    func test() -> String {
+        return "this is test"
+    }
+}
