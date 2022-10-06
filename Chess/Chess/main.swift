06a1ae91aff69a1d163887fb5305f4aaf95afd01
ChocOZerO <nayalth@gmail.com>
2022-10-06 21:50:23 +0900

    King 초기 구조 설정

--- Chess/Chess/main.swift
+++ Chess/Chess/main.swift
@@ -15,3 +15,46 @@ class Main {
         return "this is test"
     }
 }
+
+enum Color {
+    case black
+    case white
+}
+
+struct Position {
+    enum Rank {
+        case one
+        case two
+        case three
+        case four
+        case five
+        case six
+        case seven
+        case eight
+    }
+    enum File {
+        case A
+        case B
+        case C
+        case D
+        case E
+        case F
+        case G
+        case H
+    }
+    let file: File
+    let rank: Rank
+}
+protocol King {
+    var color: Color { get }
+    var position: Position { get }
+}
+
+class BlackKing: King {
+    var color: Color { .black }
+    private(set) var position: Position
+
+    init(position: Position) {
+        self.position = position
+    }
+}
