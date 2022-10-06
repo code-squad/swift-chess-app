45b52b8118cad94d976b0063fe6acebfbc0d19e3
임승혁 <seunghyeok.lim@kurlycorp.com>
2022-10-06 22:02:42 +0900

    feature: Piece, King, Color, Position, Rank, File 타입 구현

--- pair05a/pair05a/main.swift
+++ pair05a/pair05a/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+struct King: Piece {
+  let color: Color
+  var position: Position
+}
 
+enum Color {
+  case white
+  case black
+}
+
+protocol Piece {
+  var color: Color { get }
+  var position: Position { get set }
+}
+
+struct Position {
+  let rank: Rank
+  let file: File
+}
+
+enum Rank: Int {
+  case one = 1, two, three, four, five, six, sevent, eight
+}
+
+enum File: Int {
+  case A = 1, B, C, D, E, F, G, H
+}
