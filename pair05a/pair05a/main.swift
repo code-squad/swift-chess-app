ff72086a4c68b833be07f09dab1fe21f8015e367
haeseoklee <haeseoklee.dev@gmail.com>
2022-10-06 22:11:59 +0900

    feat: BlackKing, WhiteKing

--- pair05a/pair05a/main.swift
+++ pair05a/pair05a/main.swift
@@ -7,30 +7,43 @@
 
 import Foundation
 
-struct King: Piece {
-  let color: Color
-  var position: Position
+struct BlackKing: Piece {
+    var color: Color { .black }
+    var position: Position
+    
+    init(position: Position = Position(rank: Rank.one, file: File.E)) {
+        self.position = position
+    }
+}
+
+struct WhiteKing: Piece {
+    var color: Color { .white }
+    var position: Position
+    
+    init(position: Position = Position(rank: Rank.eight, file: File.E)) {
+        self.position = position
+    }
 }
 
 enum Color {
-  case white
-  case black
+    case white
+    case black
 }
 
 protocol Piece {
-  var color: Color { get }
-  var position: Position { get set }
+    var color: Color { get }
+    var position: Position { get set }
 }
 
 struct Position {
-  let rank: Rank
-  let file: File
+    let rank: Rank
+    let file: File
 }
 
 enum Rank: Int {
-  case one = 1, two, three, four, five, six, sevent, eight
+    case one = 1, two, three, four, five, six, sevent, eight
 }
 
 enum File: Int {
-  case A = 1, B, C, D, E, F, G, H
+    case A = 1, B, C, D, E, F, G, H
 }
