71a5a08bdfbe8343903f5f3abb0cc4990af51fcd
김승언[iOS개발] <seungeon.kim_12@snowcorp.com>
2022-10-06 22:09:47 +0900

    feat: king 모델 정의 및 생성

--- ChessGamePair/ChessGamePair/main.swift
+++ ChessGamePair/ChessGamePair/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+protocol Piece {
+    var color: String { get }
+    var currentLocation: Location? { get set }
+    var initialLocation: Location { get }
     
+    func movableLocations() -> [Location]
+    mutating func create()
+}
+
+struct Location: Equatable {
+    var file: String
+    var rank: Int
+}
+
+protocol King: Piece { }
+
+extension King {
+    // TODO: - empty
+    func movableLocations() -> [Location] {
+        return []
+    }
+}
+
+struct WhiteKing: King {
+    let color: String = "white"
+    var currentLocation: Location?
+    var initialLocation: Location = .init(file: "D", rank: 8)
+    
+    mutating func create() {
+        currentLocation = initialLocation
+    }
+}
+
+struct BlackKing: King {
+    let color: String = "black"
+    var currentLocation: Location?
+    var initialLocation: Location = .init(file: "D", rank: 1)
+    
+    mutating func create() {
+        currentLocation = initialLocation
+    }
+}
