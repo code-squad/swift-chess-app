fde9290d36d876ff429e3dbfcc5d89c9d46d3044
임승혁 <seunghyeok.lim@kurlycorp.com>
2022-10-10 16:48:51 +0900

    feature: BoardError 구현

---
+++ ChessApp/ChessApp/Error/BoardError.swift
@@ -0,0 +1,22 @@
+//
+//  BoardError.swift
+//  ChessApp
+//
+//  Created by MK-AM16-010 on 2022/10/10.
+//
+
+import Foundation
+
+enum BoardError: Error {
+  case notMovableFile
+  case notMovableRank
+  
+  var message: String {
+    switch self {
+    case .notMovableFile:
+      return "움직일 수 있는 File칸이 존재하지 않습니다."
+    case .notMovableRank:
+      return "움직일 수 있는 Rank칸이 존재하지 않습니다."
+    }
+  }
+}
