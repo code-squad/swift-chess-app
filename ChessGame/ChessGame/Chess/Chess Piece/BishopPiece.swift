46f780a09d3b55c5994b0acb8c60825f404e863d
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-10 13:40:03 +0900

    추가: 비숍 경로막힘 탐지

--- ChessGame/ChessGame/Chess/Chess Piece/BishopPiece.swift
+++ ChessGame/ChessGame/Chess/Chess Piece/BishopPiece.swift
@@ -17,4 +17,33 @@ struct BishopPiece: ChessPiece {
     func isDeltaValid(delta: Delta) -> Bool {
         return abs(delta.file) == abs(delta.rank)
     }
+    
+    func isMovementValid(origin: Position, destination: Position, board: ChessBoard) -> Bool {
+        
+        let fileStride = stride(from: origin.file, through: destination.file)
+        let rankStride = stride(from: origin.rank, through: destination.rank)
+        
+        var steppingPositions = zip(fileStride, rankStride)
+            .map { Position(file: $0, rank: $1) }
+        steppingPositions.removeFirst()
+        
+        for steppingPosition in steppingPositions {
+            
+            if let piece = board[steppingPosition] {
+                
+                if piece.teamColor != teamColor,
+                   destination == steppingPosition {
+                    return true
+                }
+                return false
+            }
+        }
+        
+        return true
+    }
+}
+
+func stride(from start: Int, through end: Int) -> StrideThrough<Int> {
+    let addingNumber = start < end ? 1 : -1
+    return stride(from: start, through: end, by: addingNumber)
 }
