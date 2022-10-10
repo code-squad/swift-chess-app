ec0ae6d0b8f73e09ab066caae8f7c9f1a090269f
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-10 11:41:55 +0900

    나이트 불필요한 코드 제거

--- ChessGame/ChessGame/Chess/Chess Piece/KnightPiece.swift
+++ ChessGame/ChessGame/Chess/Chess Piece/KnightPiece.swift
@@ -31,36 +31,17 @@ struct KnightPiece: ChessPiece {
     func isMovementValid(origin: Position, destination: Position, board: ChessBoard) -> Bool {
         
         let delta = destination - origin
-        let availableDeltas: [Delta]
         let steppingPosition: Position
         
         // 지나가는 위치에 다른 기물이 막고 있지 않은지 확인한다.
         if abs(delta.file) > abs(delta.rank) {
             let fileIsPositive = delta.file > 0
             steppingPosition = origin + Delta(file: fileIsPositive ? 1 : -1, rank: 0)
-            
-            availableDeltas = fileIsPositive ? [
-                Delta(file: 2, rank: 1),
-                Delta(file: 2, rank: -1)
-            ] : [
-                Delta(file: -2, rank: 1),
-                Delta(file: -2, rank: -1)
-            ]
         } else {
             let rankIsPositive = delta.rank > 0
             steppingPosition = origin + Delta(file: 0, rank: rankIsPositive ? 1 : -1)
-            
-            availableDeltas = rankIsPositive ? [
-                Delta(file: 1, rank: 2),
-                Delta(file: -1, rank: 2)
-            ] : [
-                Delta(file: 1, rank: -2),
-                Delta(file: -1, rank: -2)
-            ]
         }
         
-        guard board[steppingPosition] == nil else { return false }
-        
-        return availableDeltas.contains(delta)
+        return board[steppingPosition] == nil
     }
 }
