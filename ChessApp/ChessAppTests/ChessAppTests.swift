3e4a58a8b9707629a26fcfd73a49ceaa07572877
임승혁 <seunghyeok.lim@kurlycorp.com>
2022-10-11 23:54:11 +0900

    feature: File, Rank, Position, Piece, Coordinator 테스트 코드 추가

--- ChessApp/ChessAppTests/ChessAppTests.swift
+++ ChessApp/ChessAppTests/ChessAppTests.swift
@@ -9,69 +9,131 @@ import XCTest
 @testable import ChessApp
 
 final class ChessAppTests: XCTestCase {
+  let size: Int = 8
   var board: Board!
   
   override func setUp() {
     super.setUp()
     
-    self.board = Board(size: 8)
+    self.board = Board()
   }
   
-  func testBoardDisplay() {
-    let expectBoard = """
-                       ABCDEFGH
-                      1........
-                      2♟♟♟♟♟♟♟♟
-                      3........
-                      4........
-                      5........
-                      6........
-                      7♙♙♙♙♙♙♙♙
-                      8........
-                       ABCDEFGH
-                      """
+  // MARK: Rank Test
+  func test_valid_rank() {
+    let rank = Rank("1")
     
-    let displayBoard = self.board.display()
-    
-    XCTAssertEqual(expectBoard, displayBoard)
+    XCTAssertTrue(rank == .one)
   }
   
-  func testInitBoardDisplayScore() {
-    let expectBoardScore = "흑: 8 백: 8"
-    
-    let displayBoardScore = self.board.displayScore()
-    
-    XCTAssertEqual(expectBoardScore, displayBoardScore)
-  }
-  
-  func testChecValidationkMoviePosition() {
-    let isMovable = self.board.movePiece(from: "AA", to: "A1")
-    
-    XCTAssertFalse(isMovable)
-  }
-  
-  // MARK: Position Test
-  func test_valid_rank_get_index() {
-    let rank = Rank("B")
-    
-    XCTAssertTrue(rank == .B)
-  }
-  
-  func test_invalid_rank_get_index() {
+  func test_invalid_rank() {
     let rank = Rank("Z")
     
     XCTAssertNil(rank)
   }
   
-  func test_valid_file_get_index() {
-    let file = File("1")
+  func test_display_rank() {
+    let rank = Rank(2)
     
-    XCTAssertTrue(file == .one)
+    XCTAssertEqual(rank!.toDisplay, "2")
   }
   
-  func test_invalid_file_get_index() {
+  // MARK: File Test
+  func test_valid_file() {
+    let file = File("A")
+    
+    XCTAssertTrue(file == .A)
+  }
+  
+  func test_invalid_file() {
     let file = File("9")
     
     XCTAssertNil(file)
+  }
+  
+  func test_display_file() {
+    let file = File("B")
+    
+    XCTAssertEqual(file!.toDisplay, "B")
+  }
+  
+  // MARK: Position Test
+  func test_position_coordinate_index() {
+    let position = Position(file: File("A")!, rank: Rank("1")!)
+    
+    let indexs = position.toCoordinate()
+    
+    XCTAssertEqual(0, indexs.0)
+    XCTAssertEqual(0, indexs.1)
+  }
+  
+  func test_coordinate_move_file() {
+    let position = Position(file: File("A")!, rank: Rank("1")!)
+    
+    let movedFile = position.move(fileIndex: 1)
+    
+    XCTAssertEqual(movedFile!, .B)
+  }
+  
+  func test_coordinate_move_rank() {
+    let position = Position(file: File("A")!, rank: Rank("1")!)
+    
+    let movedRank = position.move(rankIndex: 1)
+    
+    XCTAssertEqual(movedRank!, .two)
+  }
+  
+  // MARK: Piece Test
+  func test_get_blackpawn_shape() {
+    let blackPawn = Pawn(color: .black)
+    
+    XCTAssertEqual(blackPawn.shape, "♟")
+  }
+  
+  func test_get_whitepawn_shape() {
+    let whitePawn = Pawn(color: .white)
+    
+    XCTAssertEqual(whitePawn.shape, "♙")
+  }
+  
+  func test_get_empty_shape() {
+    let empty = Empty()
+    
+    XCTAssertEqual(empty.shape, ".")
+  }
+  
+  // MARK: PositionCoordinator Test
+  func test_reset_blackPawn() {
+    let coordinator = PositionCoordinator(positionSize: self.size)
+    coordinator.reset()
+    
+    let piece = coordinator.getPiece(indexs: (2, 0))
+    let pawn = Pawn(color: .black)
+    
+    XCTAssertEqual(pawn.shape, "♟")
+    XCTAssertEqual(pawn.color, .black)
+  }
+  
+  func test_reset_whitePawn() {
+    let coordinator = PositionCoordinator(positionSize: self.size)
+    coordinator.reset()
+    
+    let piece = coordinator.getPiece(indexs: (7, 0))
+    let pawn = Pawn(color: .white)
+    
+    XCTAssertEqual(pawn.shape, "♙")
+    XCTAssertEqual(pawn.color, .white)
+  }
+  
+  func test_set_piece() {
+    let coordinator = PositionCoordinator(positionSize: self.size)
+    coordinator.reset()
+    
+    let emptyPiece = Empty()
+    let pawn = coordinator.getPiece(indexs: (7, 0))
+    XCTAssertNotNil(pawn as? Pawn)
+    
+    coordinator.setPiece(emptyPiece, indexs: (7, 0))
+    let empty = coordinator.getPiece(indexs: (7, 0))
+    XCTAssertNotNil(empty as? Empty)
   }
 }
