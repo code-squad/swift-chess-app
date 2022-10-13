9445caa3482225452ca16fa5caba2127b89b66fd
김승언[iOS개발] <seungeon.kim_12@snowcorp.com>
2022-10-13 11:46:51 +0900

    refac: 모델별 상수 선언 위치 변경

--- swift-chess-app/swift-chess-app/Utills/Dimension.swift
+++ swift-chess-app/swift-chess-app/Utills/Dimension.swift
@@ -11,12 +11,4 @@ enum Dimension {
     enum Board {
         static let length = 8
     }
-    
-    enum Point {
-        static let pawn = 1
-        static let bishop = 3
-        static let knight = 3
-        static let rook = 5
-        static let queen = 9
-    }
 }
